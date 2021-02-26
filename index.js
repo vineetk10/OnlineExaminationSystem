cb2021778efc9e1d6cba1eaec8d01618860bfc34
vineetkarmiani <confiable.vineet@gmail.com>
2021-02-26 23:19:32 +0530

    Added dotenv as it will help us not deploy sensitive material on production

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/exampaperbuilder', {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true
 }).then(()=>{
     console.log("DB CONNECTED");
-})
+}).catch(console.log("DB CONNECTED FAILED"))
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, ()=>{
     console.log(`app is running at ${port}`);

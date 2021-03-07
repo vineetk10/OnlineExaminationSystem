2b8fb07b065bb5bb151f63f5c6d96f062f520d01
Vineet karmiani <vineetkarmiani@gmail.com>
2021-03-07 15:44:04 +0530

    Added validation to the routes using express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var {signout, signup} = require("../controllers/auth");
+const { check , validationResult } = require('express-validator');
 
-router.post('/signup',signup);
+router.post('/signup',[
+    check("firstName","name should be at least 3 characters").isLength({ min: 3 }),
+    check("email","email is incorrect").isEmail(),
+    check("password")
+        .isLength({ min: 6 })
+        .withMessage('password must be at least 6 chars long')
+        .matches(/\d/)
+        .withMessage('must contain a number')
+],
+signup);
 router.post('/signout',signout);
 
 module.exports = router;

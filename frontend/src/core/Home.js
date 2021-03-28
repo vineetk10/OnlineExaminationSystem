848c252a995cf51c6336ff1b473ee2e373f5f81e
Vineet karmiani <vineetkarmiani@gmail.com>
2021-03-28 21:36:57 +0530

    Using css file

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+import "../styles.css";
 const Home = ()=>{
     return (
         <div>
-            <h1>Hello Front End</h1>
+            <h1 className="text-white">Hello Front End</h1>
         </div>
     )
 }

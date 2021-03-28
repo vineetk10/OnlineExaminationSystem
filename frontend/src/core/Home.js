8ce40d6ad6a37c1dee30b512962c0e8a5e6df8dd
Vineet karmiani <vineetkarmiani@gmail.com>
2021-03-29 00:10:22 +0530

    Creating base component and injecting it in home page

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "../styles.css";
+import Base from './Base';
 const Home = ()=>{
     return (
-        <div>
-            <h1 className="text-white">Hello Front End</h1>
-        </div>
+        <Base title="Online Examination" description="An easy way to give exams and monitor your results">
+           
+        </Base>
     )
 }
 

f9e03c5922d22a339dbdbffcc75978842e3a5781
Vineet karmiani <vineetkarmiani@gmail.com>
2021-04-03 22:07:01 +0530

    creating new route for creating paper template

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Home from "./core/Home"
-
+import CreateTemplate from "./admin/CreateQuestionPaperTemplate.js"
 const Routes = ()=>{
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home}></Route>
+                <Route path="/admin/createTemplate" exact component={CreateTemplate}></Route>
             </Switch>
         </BrowserRouter>
     )
---
+++ frontend/src/admin/CreateQuestionPaperTemplate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateTemplate = ()=>{
+    return (
+        <h1>Hi</h1>
+    )   
+}
+
+export default CreateTemplate

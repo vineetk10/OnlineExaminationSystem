ba36cbcf6f7c8871d41ab13d9f8f4ddeb89da22f
Vineet karmiani <vineetkarmiani@gmail.com>
2021-04-27 21:58:49 +0530

    Solving cors issue

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`app is running at ${port}`);
 })
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 //Middlewares
 app.use(express.json());
 app.use(cookieParser());

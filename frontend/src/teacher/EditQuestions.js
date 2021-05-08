da6c5631ddd49deb99abd6e100d2aa9571f6cf41
Vineet karmiani <vineetkarmiani@gmail.com>
2021-05-08 18:13:01 +0530

    To display the survey, but will have to change and show the tempate

--- frontend/src/teacher/EditQuestions.js
+++ frontend/src/teacher/EditQuestions.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Base from '../core/Base';
 import QuestionMenu from './QuestionMenu';
+import * as Survey from "survey-react";
 import "survey-react/survey.css";
 const EditQuestion= () => {
 
   
 
+  const json = {
+    elements: [
+     { type: "text", name: "customerName", title: "What is your name?", isRequired: true}
+    ]
+   };
+  
+   var model = new Survey.Model(json);
+   //Define a callback methods on survey complete
+   const onComplete=(survey, options) =>{
+    //Write survey results into database
+    console.log("Survey results: " + JSON.stringify(survey.data));
+   }
+   
   return (
     <Base title="edit questions" description="">
       <QuestionMenu/>
-      <div id="surveyContainer">
-                <div id="creatorElement"></div>
-            </div>
+      <Survey.Survey model={model} onComplete={onComplete}/>
     </Base>
   )
 }

e575bd5574045139fde3be022905d019756905fa
Vineet karmiani <vineetkarmiani@gmail.com>
2021-05-08 17:16:19 +0530

    Working state of craetor in edit qsns

--- frontend/public/surveyCreator.js
+++ frontend/public/surveyCreator.js
@@ -1,3 +1,4 @@
+
 var options = {
     showLogicTab: true
 };
@@ -9,3 +10,19 @@ creator.showToolbox = "right";
 creator.showPropertyGrid = "right";
 //Make toolbox active by default
 creator.rightContainerActiveItem("toolbox");
+
+var localStorageName = "SaveLoadSurveyCreatorExample";
+//Setting this callback will make visible the "Save" button
+creator.saveSurveyFunc = function (saveNo, callback) {
+    //save the survey JSON
+    console.log(creator.text);
+    //You can store in your database JSON as text: creator.text  or as JSON: creator.JSON
+    window
+        .localStorage
+        .setItem(localStorageName, creator.text);
+
+    //We assume that we can't get error on saving data in local storage
+    //Tells creator that changing (saveNo) saved successfully.
+    //Creator will update the status from Saving to saved
+    callback(saveNo, true);
+}
--- frontend/src/teacher/EditQuestions.js
+++ frontend/src/teacher/EditQuestions.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Base from '../core/Base';
 import QuestionMenu from './QuestionMenu';
-
+import "survey-react/survey.css";
 const EditQuestion= () => {
 
+  
 
   return (
     <Base title="edit questions" description="">
       <QuestionMenu/>
+      <div id="surveyContainer">
+                <div id="creatorElement"></div>
+            </div>
     </Base>
   )
 }

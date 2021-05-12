7d0dc1ae9d9137817e8b494e23c6b053096e9b82
Adityassinghania <adityassinghania@gmail.com>
2021-05-13 01:49:08 +0530

    improves foooter postioning

--- frontend/src/core/Base.js
+++ frontend/src/core/Base.js
@@ -6,17 +6,17 @@ const Base = ({
     className = "text-white p-2",
     children
 })=>(
-    <div>
-       <div className="container-fluid">
-            <div className="jumbotron bg-light text-warning text-left p-1 rounded">
+    <div className="page-container">
+       <div className="content-wrap">
+            <div className="jumbotron bg-white text-warning text-left p-2">
                 <h2>{title}</h2>   
-                <p>{description}</p> 
+                <p>{description}</p>
             </div>  
             <div className={className}>{children}</div>
         </div> 
-        <footer className="footer bg-light mt-auto py-3">
+        <footer className="footer bg-light mt-auto">
             <div className="row">
-            <div className="col-md-8 offset-md-2">                
+            <div className="col-md-12">                
             <div className="container-fluid bg-light text-dark text-center">
                 <p>If you have any questions, feel free to reach out!</p>
                 <button className="btn btn-outline-warning btn-sm rounded">Contact Us</button>

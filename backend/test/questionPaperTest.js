e8c7a29f58d52ff9ff53ae8256802982fabb0919
Vineet karmiani <vineetkarmiani@gmail.com>
2021-07-03 17:02:40 +0530

    Wrote 1 test and added after(cleanup)

--- backend/test/questionPaperTest.js
+++ backend/test/questionPaperTest.js
@@ -12,43 +12,45 @@ var questionPapers = rewire('../controllers/questionPaper');
 var GetAllQuestionPapersOfUser = questionPapers.__get__('GetAllQuestionPapersOfUser');
 describe('Task API Routes', function() {
     // This function will run before every test to clear database
-    questionPaper = QuestionPaper.create({
-        paperTitle:"Marathi",
-        subject: "Marathi",
-        duration:"120",
-        maxMarks:"100",
-        createdBy: "604e0f96a224d944b89ef730", 
-        questions: "t@t.com",  
-        password: "test"})
-        .then(() => console.log("Insert Successful")) 
-        .catch((err)=>console.log(err))
-
-    // Test for ajax call but too tedious and difficult
-    // describe('GET /tasks', function() {
-    //     it('returns a list of tasks', function(done) {
-    //         chai.request(app)
-    //         .post(`/api/questionPapers/${userId}`)
-    //         .set("Accept","application/json")
-    //         .set("Content-Type", "application/json")
-    //             .send({
-    //                 "userId": "604e0f96a224d944b89ef730"
-    //              })
-    //             .end(function(err, res) {
-    //                 console.log(res);
-    //                 expect(res.body).to.have.lengthOf(1);
-    //                 done(err);
-    //             });
-    //     });
-    // });
-
+    
     describe('GET ALL QUESTION PAPERS', function() {
-        it('returns number of papers', async function() {
+
+        before(function() { 
+            QuestionPaper.create({
+                paperTitle:"Marathi",
+                subject: "Marathi",
+                duration:"120",
+                maxMarks:"100",
+                createdBy: "604e0f96a224d944b89ef730", 
+                questions: "t@t.com",  
+                password: "test"
+            })
+            .then(() => console.log("Insert Successful")) 
+            .catch((err)=>console.log(err))
+        })
+    
+        after(()=>{
+            QuestionPaper.deleteOne({paperTitle:"Marathi",subject: "Marathi"})
+            .then(() => console.log("Delete Successful"))
+            .catch((err)=>console.log(err))
+        });
+
+        it('returns number of papers when userId correct', async function() {
             var papers = await GetAllQuestionPapersOfUser({
                 "query": {},
                 "body" : {"userId":"604e0f96a224d944b89ef730"}
             })
 
-            expect(papers.length).to.equal(8);
+            expect(papers.length).to.equal(1);
+        });
+
+        it('returns 0 number of papers when userId incorrect', async function() {
+            var papers = await GetAllQuestionPapersOfUser({
+                "query": {},
+                "body" : {"userId":"604e0f96a224d944b89ef731"}
+            })
+
+            expect(papers.length).to.equal(0);
         });
     });
 
